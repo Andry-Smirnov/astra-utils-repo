5391dd3007e834bc5c7d836699369961a0258ca8
Andry Smirnov <Andry.Smirnov@gmail.com>
2026-07-29 11:23:25 +0300

    Обновит fpcupdeluxe.app/Lazarus_fpcupdeluxe.sh

--- fpcupdeluxe.app/Lazarus_fpcupdeluxe.sh
+++ fpcupdeluxe.app/Lazarus_fpcupdeluxe.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 # fpcupdeluxe: home startlink script
-"/home/virus/fpcupdeluxe/fpcupdeluxe/lazarus/lazarus" --pcp="/home/virus/fpcupdeluxe/fpcupdeluxe/config_lazarus" "$@"
+"~/fpcupdeluxe/fpcupdeluxe/lazarus/lazarus" --pcp="~/fpcupdeluxe/fpcupdeluxe/config_lazarus" "$@"
