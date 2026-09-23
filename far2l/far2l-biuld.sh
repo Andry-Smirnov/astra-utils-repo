98f3972e00165887f9faf3a43586274ea84030a7
Andry Smirnov <Andry.Smirnov@gmail.com>
2026-09-23 13:04:05 +0300

    Обновит far2l/far2l-biuld.sh

--- far2l/far2l-biuld.sh
+++ far2l/far2l-biuld.sh
@@ -13,7 +13,7 @@ CYAN='\033[0;36m'
 
 printf "${MSGCOLOR}Установщик менеджера файлов far2l${NOCOLOR}\n"
 
-sudo apt install gawk m4 libx11-dev libxi-dev libxerces-c-dev libspdlog-dev libuchardet-dev libssh-dev libssl-dev  -y
+sudo apt install gawk m4 libusb-1.0-0-dev libx11-dev libxi-dev libxerces-c-dev libspdlog-dev libuchardet-dev libssh-dev libssl-dev  -y
 sudo apt install libsmbclient-dev libnfs-dev libneon27-dev libarchive-dev cmake g++ git -y
 sudo apt install libglib2.0-dev libwxgtk3.0-dev libgtkmm-3.0-dev uuid-dev libsecret-1-dev sshfs -y
 sudo apt install gvfs-libs gvfs-backends gvfs-fuse -y
